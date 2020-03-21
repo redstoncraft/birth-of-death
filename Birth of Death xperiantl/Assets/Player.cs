98be60506539e7c8209ebaed775f714bf257e701
redstoncraft <61513634+redstoncraft@users.noreply.github.com>
2020-03-21 17:30:36 +0100

    Saver

--- Birth of Death xperiantl/Assets/Player.cs
+++ Birth of Death xperiantl/Assets/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -73,6 +74,25 @@ public class Player : MonoBehaviour
         {
             SceneManager.LoadScene(strSceneName);
         }
+
+        if (col.CompareTag("Save"))
+        {
+            SaveSystem.SavePlayer(this);
+                
+        }
+    }
+
+    void Awake()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        currentHealth = data.health;
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
     }
 
 }
